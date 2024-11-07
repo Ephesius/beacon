3c09f9b5374b030aea1ccc62c6e48a3ec7959fe9
Justin Hardin <jnhardin85@gmail.com>
2024-11-07 16:11:24 -0600

    Added MainPage view and its code-behind

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,8 +1,23 @@
-﻿namespace Beacon
-{
-    public partial class MainPage : ContentPage
-    {
+﻿using Beacon.ViewModels;
 
+namespace Beacon;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
     }
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // Lock to portrait orientation as per spec
+        if (DeviceInfo.Platform == DevicePlatform.iOS || DeviceInfo.Platform == DevicePlatform.Android)
+        {
+            Microsoft.Maui.Controls.Application.Current!.MainPage!.Rotation = 0;
+        }
+    }
 }
