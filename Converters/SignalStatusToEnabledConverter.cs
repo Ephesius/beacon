0dbcb29c7bd98f30162c6a024e4eaf7f748eff18
Justin Hardin <jnhardin85@gmail.com>
2024-11-07 16:28:28 -0600

    Added SignalStatusToEnabledConverter

---
+++ Converters/SignalStatusToEnabledConverter.cs
@@ -0,0 +1,22 @@
+﻿using Beacon.Models;
+using System.Globalization;
+
+namespace Beacon.Converters;
+
+public class SignalStatusToEnabledConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is GPSStatus status)
+        {
+            return status == GPSStatus.Strong;
+        }
+
+        return false;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
