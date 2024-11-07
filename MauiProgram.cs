dd8565b5d7313d51bf83b7f78a200758cb72130a
Justin Hardin <jnhardin85@gmail.com>
2024-11-07 16:38:47 -0600

    Registered MainPage view and converters

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Beacon.Services;
+using Beacon.ViewModels;
+using Beacon.Converters;
 using Microsoft.Extensions.Logging;
 
 namespace Beacon
@@ -24,11 +26,30 @@ namespace Beacon
             builder.Services.AddSingleton(Geolocation.Default);
             builder.Services.AddSingleton(Compass.Default);
 
+            // Register ViewModels
+            builder.Services.AddSingleton<MainViewModel>();
+
+            // Register Pages
+            builder.Services.AddSingleton<MainPage>();
+
+            RegisterConverters();
+
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
+        }
+
+        private static void RegisterConverters()
+        {
+            if (Application.Current?.Resources != null)
+            {
+                var resources = Application.Current.Resources;
+                resources["SignalStatusToColorConverter"] = new SignalStatusToColorConverter();
+                resources["StateToVisibilityConverter"] = new StateToVisibilityConverter();
+                resources["SignalStatusToEnabledConverter"] = new SignalStatusToEnabledConverter();
+            }
         }
     }
 }
