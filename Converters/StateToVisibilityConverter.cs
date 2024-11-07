88ba0172283cb81030219874e9890697d1c27936
Justin Hardin <jnhardin85@gmail.com>
2024-11-07 16:22:35 -0600

    Added StateToVisibilityConverter

---
+++ Converters/StateToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using Beacon.Models;
+using System.Globalization;
+
+namespace Beacon.Converters;
+
+public class StateToVisibilityConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is AppState currentState && parameter is AppState targetState)
+        {
+            return currentState == targetState;
+        }
+
+        return false;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
