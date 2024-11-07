61151a3afd3d9f790c321704f93ec0b9eeff4ced
Justin Hardin <jnhardin85@gmail.com>
2024-11-07 16:18:42 -0600

    Added SignalStatusToColorConverter to convert signal strenth to corresponding color

---
+++ Converters/SignalStatusToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using Beacon.Models;
+using System.Globalization;
+
+namespace Beacon.Converters;
+
+public class SignalStatusToColorConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is GPSStatus status)
+        {
+            return status switch
+            {
+                GPSStatus.Strong => Colors.Green,
+                GPSStatus.Weak => Colors.Yellow,
+                GPSStatus.None => Colors.Red,
+                _ => Colors.Gray
+            };
+        }
+
+        return Colors.Gray;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
